3f903609b8ea1125e9cb5e7606246ef844573366
cristinaradulescu <cristina.radulescu@metrosystems.net>
2020-10-13 19:43:20 +0300

    New change

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
 
     public static void main( String[] args ) {
 
-        System.out.println( "This is my last change" );
+        System.out.println( "New change" );
 
     }
 }
