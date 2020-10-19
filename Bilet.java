6ab50a08a51959378cf6300d1948b9198500e722
gabriel-mihaila <72153726+gabriel-mihaila@users.noreply.github.com>
2020-10-20 02:24:10 +0300

    Add files via upload

---
+++ Bilet.java
@@ -0,0 +1,11 @@
+package com.codegym.ex5;
+
+public class Bilet {
+    private int numere[]=new int[6];
+    public Bilet(int numere[]){
+        this.numere=numere;
+    }
+    public int[] getNumere(){
+        return this.numere;
+    }
+}
